bc4d008331e082342e0f800a263682a028d9adf8
lgal1 <dlgaller@gmail.com>
2025-08-18 08:01:27 -0400

    Fixed step in the input for data_split in main.py and created split for trainval and test verified in Wandb

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def go(config: DictConfig):
                 version="main",
                 env_manager="conda",
                 parameters = {
-                    "input_artifact": "clean_sample.csv:latest",
+                    "input": "clean_sample.csv:latest",
                     "test_size": config["modeling"]["test_size"],
                     "random_seed": config["modeling"]["random_seed"],
                     "stratify_by": config["modeling"]["stratify_by"],
